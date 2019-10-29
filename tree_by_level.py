bfae1eb62df6f6ef5797f831100a35df09b82ff0
naomatheus <pacificrobinson@gmail.com>
2019-10-29 09:57:17 -0500

    added tree problem

---
+++ tree_by_level.py
@@ -0,0 +1,31 @@
+# You are given a tree, and your job is to print it level-by-level with linebreaks.
+
+# The output should be
+# a
+# bc
+# defg
+
+# Here's a starting point:
+from collections import deque
+
+class Node(object):
+ def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+    def __str__(self):
+        # Fill this in.
+        return
+
+tree = Node('a')
+tree.left = Node('b')
+tree.right = Node('c')
+tree.left.left = Node('d')
+tree.left.right = Node('e')
+tree.right.left = Node('f')
+tree.right.right = Node('g')
+    
+print(tree)
+# a
+# bc
+# defg

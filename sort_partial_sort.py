80c718da6894503d09bec5215fb72fb985c4f600
naomatheus <pacificrobinson@gmail.com>
2019-10-31 11:15:46 -0500

    added merge sort recursion structure

--- sort_partial_sort.py
+++ sort_partial_sort.py
@@ -11,11 +11,48 @@
 
 # Here's a starting point:
 
-def sort_partially_sorted(nums, k):
+def sort_partially_sorted(list, k):
  # Fill this in
+    print('splitting...')
     
-    return sorted(nums)
+    if len(list) > 1:
+        mid = len(list)//2
+        lefthalf = list[:mid]
+        righthalf = list[mid:]
+    print(lefthalf,righthalf)
+
+# recursion
+    sort_partially_sorted(lefthalf,2)
+    sort_partially_sorted(righthalf,2)
+
+    i=0
+    j=0
+    k=0
+    new_list = []
+    while i < len(lefthalf) and j > len(righthalf):
+    # while split lists are > 0 length
+    # compare each of the elements and group them
+        if lefthalf[i] < righthalf[j]:
+            new_list[k]=lefthalf[i]
+            i=i+1
+        else:
+            new_list[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    while i < len(lefthalf):
+        new_list[k] = lefthalf[i]
+        i=i+1
+        k=k+1
+    while j < len(righthalf):
+        new_list[k]=righthalf[j]
+        j=j+1
+        k=k+1
     
+    print('merging...',new_list)
+
+     
+
+    sort_partially_sorted(list)
 
 print(sort_partially_sorted([3, 2, 6, 5, 4],2))
 # [2,3,4,5,6]

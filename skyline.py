fe00b40da070f8dd2828072f2df1c672785050a2
naomatheus <pacificrobinson@gmail.com>
2019-11-04 17:01:22 -0600

    added skyline problem

---
+++ skyline.py
@@ -0,0 +1,16 @@
+# This problem was recently asked by Facebook:
+# Given a list of buildings in the form of (left, right, height), return what the skyline should look like. 
+# The skyline should be in the form of a list of (x-axis, height), where xaxis is the next point where there is a change in height starting from 0, and height is the new height starting from the x-axis.
+
+def generate_skyline(buildings):
+#        Fill this in.
+#            2 2 2
+#            2 2 2
+#        1 1 2 2 2 1 1
+#        1 1 2 2 2 1 1
+#        1 1 2 2 2 1 1
+# pos: 1 2 3 4 5 6 7 8 9
+    return
+print('generate_skyline([(2, 8, 3)')
+
+# Output: [(2, 3), (4, 5), (7, 3), (9, 0)]

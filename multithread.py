8ca6122280265f18702082659d20b2f1078b84d9
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-11-12 07:06:13 -0600

    added newer threading module helpers

--- multithread.py
+++ multithread.py
@@ -6,6 +6,8 @@ import time
 
 #Define a function from the thread 
 
+# This code executes an infinite loop, and will need to be keyboard interrupted (ctrl + c)
+
 def print_time(thread_name,delay):
 	count = 0
 	while count < 5:
@@ -13,7 +15,10 @@ def print_time(thread_name,delay):
 		count += 1
 		print( '%s: %s:' % (thread_name, time.ctime(time.time() ) ))
 
-	# return
+	if count >= 6:
+
+		return 'done'
+	return 'complete'
 
 try: 
 	_thread.start_new_thread( print_time, ("Thread-1",1))
@@ -24,5 +29,31 @@ except Exception as err:
 	print(err.args)
 	raise
 
-while 1:
+while True:
 	pass
+
+# Although it is very effective for low-level threading, the thread module is very limited compared to the newer threading module.
+
+# Threading module
+
+# threading.activeCount() − Returns the number of thread objects that are active.
+
+# threading.currentThread() − Returns the number of thread objects in the caller's thread control.
+
+# threading.enumerate() − Returns a list of all thread objects that are currently active.
+
+# run() − The run() method is the entry point for a thread.
+
+# start() − The start() method starts a thread by calling the run method.
+
+# join([time]) − The join() waits for threads to terminate.
+
+# isAlive() − The isAlive() method checks whether a thread is still executing.
+
+# getName() − The getName() method returns the name of a thread.
+
+# setName() − The setName() method sets the name of a thread.
+
+
+
+

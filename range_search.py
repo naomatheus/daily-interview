de6c5a1eb1c81c3d682c840368f64b9732d3a64b
matthew Robinson <matthewrobinson@matthews-Air-3.attlocal.net>
2020-02-14 12:20:07 -0600

    solve for range search

--- range_search.py
+++ range_search.py
@@ -5,41 +5,37 @@
 
 case = [1,1,3,5,7]
 tar = 1
+case2 = [1,2,3,4]
+tar2 = 5
 
 def find_num(nums,target):
-
-
 	high = 0
 	low = 0
-	## find and match target, find the first element in nums that matches target 
+	# enumerate the array to find (x,y) of range
 	for enum,n in enumerate(nums):
-		print(enum,n)
-		
+		if target not in nums:
+			return (-1,-1)
+		# base, if target not in array, return (-1,-1)
 
-
-		if n == tar:
+		if n == target:
 			low = enum
-			# print(low)
 			if low == 1:
 				low-=1
-				# print(low)
+		""" if pass of base, 
+		the index enum containing the target element minus one is the low end of inclusive range """
 
-		if n != tar:
+		if n != target:
 			high = enum - 1
 			break
-			# if n != tar:
-				# high = enum - 1
-
-
-
-	#the index containing the target element minus one is the low end of inclusive range
-
-	## the index containing the target element plus one is the high end of inclusive range
+				
+	""" the index containing the target element plus one is the high end of inclusive range """
 
 	return (low,high)
 
 print(find_num(case,tar))
+print(find_num(case2,tar2))
 
+## EXAMPLES WITH EXPECTED OUTPUT
 # print(find_num([1,1,3,5,7],1))
 # (0,1)
 
